1d65cfed9653d36124adc72c9095e90d28a8cccd
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-09-10 16:17:56 -0700

    Cleaning up code and change the file path for the source file.
    Lastly, just updated the table comment so it specified import date and source URL.
    
    I DID NOT repush the table, only the comment. Table looks good

--- Data Prep/fire_perimeter_data_import.R
+++ Data Prep/fire_perimeter_data_import.R
@@ -21,7 +21,7 @@ options(scipen = 999)
 #Raw Data Source URL: https://gis.data.ca.gov/datasets/CALFIRE-Forestry::california-fire-perimeters-all/explore?filters=eyJGSVJFX05BTUUiOlsiRUFUT04iXX0%3D&location=34.238755%2C-117.902524%2C10.00
 
 #downloaded here
-fire_shp <- st_read("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\California_Historic_Fire_Perimeters_5734478986827458227\\California_Fire_Perimeters_(all).shp")
+fire_shp <- st_read("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\CA Fire Perimeters Cal FIRE eGIS\\California_Fire_Perimeters_(all).shp")
 
 #check SRID
 st_crs(fire_shp) #3857
@@ -40,18 +40,14 @@ fire_shp_3310 <- st_transform(eaton, 3310)
 
 #### Step 2: Use export function to push to postgres ####
 
-# export_shpfile(con=con, df=fire_shp_4326, schema="data", 
-#                table_name= "cal_fire_prmtr_4326", 
-#                geometry_column = "geometry")
 
 export_shpfile(con=con, df=fire_shp_3310, schema="data",
                table_name= "eaton_fire_prmtr_3310",
                geometry_column = "geometry")
 
-# dbSendQuery(con, "COMMENT ON TABLE data.cal_fire_prmtr_4326 IS 
-#             'Fire Perimeter data from CAL FIRE eGIS, July 20, 2025 in SRID 4326. 
-#             QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fire_perimeter.docx'")
 
 dbSendQuery(con, "COMMENT ON TABLE data.eaton_fire_prmtr_3310 IS
             'Fire Perimeter data for the Eaton Fire from CAL FIRE eGIS, July 20, 2025 in SRID 3310
-            QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fire_perimeter.docx'")
+            Imported on 8-29-25
+            QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fire_perimeter.docx
+            For definitions, see source: https://gis.data.ca.gov/datasets/CALFIRE-Forestry::california-fire-perimeters-all/explore'")
