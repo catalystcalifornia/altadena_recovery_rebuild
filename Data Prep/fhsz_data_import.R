afb839e3dbcc8f994699458f817ab55f5393d828
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-09-11 13:12:59 -0700

    Changed relative file path to central folder for the fire hazard severity zones
    Moved to using state shapefile for import, but still had to remove the 3d geom
    
    Re-exported the state shapefile to postgres
    
    Added table column comments and revised table comments for both the state and local files
    
    Added leaflet code as part of QA to make sure shapes end up working for visualizing

--- Data Prep/fhsz_data_import.R
+++ Data Prep/fhsz_data_import.R
@@ -21,8 +21,8 @@ options(scipen = 999)
 #Raw Data Source URL: https://osfm.fire.ca.gov/what-we-do/community-wildfire-preparedness-and-mitigation/fire-hazard-severity-zones
 
 #downloaded here
-state_shp <- st_read("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\SRA FHSZ Data Effective 04.01.2024.shp")
-local_shp <- st_read("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\LRA FHSZ Combined Data 2025.shp")
+state_shp <- st_read("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\OSFM Fire Hazard Severity Zones\\FHSZSRA_23_3\\FHSZSRA_23_3.shp")
+local_shp <- st_read("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\OSFM Fire Hazard Severity Zones\\LRA FHSZ Combined Data 2025.shp")
   
 #check SRID
 st_crs(state_shp) #3310
@@ -32,23 +32,96 @@ st_crs(local_shp) #3310
 colnames(state_shp) <- tolower(colnames(state_shp))
 colnames(local_shp) <- tolower(colnames(local_shp))
 
+# force to 2d geom for state
+state_shp <- st_zm(state_shp)
 
 #### Step 2: Use export function to push to postgres ####
+# state
+# export_shpfile(con=con, df=state_shp, schema="data",
+#                table_name= "state_fhsz_3310",
+#                geometry_column = "geometry")
 
-export_shpfile(con=con, df=state_shp, schema="data",
-               table_name= "state_fhsz_3310",
-               geometry_column = "geometry")
+# Comment on table and columns
+# schema <- "data"
+# table_name <- "state_fhsz_3310"
+# indicator <- "Data on STATE Fire Hazard Severity Zones sourced from the CAL FIRE Office of State Fire Marshall in SRID 3310
+#             Fire zones for state responsibility areas
+#             Imported on 9-11-25
+#             Notes: The Fire Hazard Severity Zone map evaluates “hazard,” not “risk”. “Hazard” is based on the physical conditions that create a likelihood and expected fire behavior over a 30 to 50-year period without considering mitigation measures such as home hardening, recent wildfire, or fuel reduction efforts. “Risk” is the potential damage a fire can do to the area under existing conditions, accounting for any modifications such as fuel reduction projects, defensible space, and ignition resistant building construction."
+# source <- "Source: https://osfm.fire.ca.gov/what-we-do/community-wildfire-preparedness-and-mitigation/fire-hazard-severity-zones"
+# qa_filepath <- " QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fhsz.docx"
+# column_names <- colnames(state_shp) # Get column names
+# column_names <- column_names[column_names != "geometry"] # remove geometry column name from list throws an error when adding column comments
+# column_comments <- c(
+#   "state responsibility area",
+#   "Fire hazard severity zone numbers 1 to 3",
+#   "Fire hazard severity zone description  moderate, high, very high"
+# )
+# 
+# add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
 
-dbSendQuery(con, "COMMENT ON TABLE data.state_fhsz_3310 IS
-            'Data on STATE Fire Hazard Severity Zones sourced from the CAL FIRE in SRID 3310
-            QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fhsz.docx'")
-#FAILED ABOVE
+# local
+# export_shpfile(con=con, df=local_shp, schema="data",
+#                table_name= "local_fhsz_3310",
+#                geometry_column = "geometry")
 
-export_shpfile(con=con, df=local_shp, schema="data",
-               table_name= "local_fhsz_3310",
-               geometry_column = "geometry")
+# Comment on table and columns
+# schema <- "data"
+# table_name <- "local_fhsz_3310"
+# indicator <- "Data on LOCAL Fire Hazard Severity Zones sourced from the CAL FIRE Office of State Fire Marshall in SRID 3310
+#             Fire zones for local responsibility areas as of March 2025
+#             Imported on 9-11-25
+#             Notes: The Fire Hazard Severity Zone map evaluates “hazard,” not “risk”. “Hazard” is based on the physical conditions that create a likelihood and expected fire behavior over a 30 to 50-year period without considering mitigation measures such as home hardening, recent wildfire, or fuel reduction efforts. “Risk” is the potential damage a fire can do to the area under existing conditions, accounting for any modifications such as fuel reduction projects, defensible space, and ignition resistant building construction. 
+#             These are the local fire zones as of March 2025 Phases 1-4 the OSFM has been completing local fire zones in phases, which seems complete"
+# source <- "Source: https://osfm.fire.ca.gov/what-we-do/community-wildfire-preparedness-and-mitigation/fire-hazard-severity-zones"
+# qa_filepath <- " QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fhsz.docx"
+# column_names <- colnames(local_shp) # Get column names
+# column_names <- column_names[column_names != "geometry"]
+# column_comments <- c(
+#   "local responsibility area",
+#   "Fire hazard severity zone numbers 1 to 3 and separate category of -3",
+#   "Fire hazard severity zone description  moderate, high, very high, -3 represents non-wildland, or areas that dont have any direct threat, non-wildland areas further from distance to wildland increases have lower hazard scores compared to those adjacent to wildland"
+# )
+# add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
 
 
-dbSendQuery(con, "COMMENT ON TABLE data.local_fhsz_3310 IS
-            'Data on LOCAL Fire Hazard Severity Zones sourced from the CAL FIRE in SRID 3310
-            QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_import_fhsz.docx'")
+#### Step 3: Test export ---
+library(leaflet)
+library(htmlwidgets)
+
+local_zones <- st_read(con, query="SELECT * FROM data.local_fhsz_3310", geom="geom")
+
+local_zones <- local_zones %>% filter(fhsz>=1) %>% st_transform(4326)
+
+colorpal <- colorFactor(palette = "YlOrRd", domain = local_zones$fhsz_descr)
+
+# map zones by color to check import and conversion
+leaflet () %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(data=local_zones,
+              fillColor=~colorpal(fhsz_descr),
+              fillOpacity=0.7,
+              color="white",
+              weight=1,
+              popup=~fhsz_descr)
+# checks out
+
+state_zones <- st_read(con, query="SELECT * FROM data.state_fhsz_3310", geom="geom")
+
+state_zones <- state_zones %>% st_transform(4326)
+st_crs(state_zones)
+
+colorpal <- colorFactor(palette = "YlOrRd", domain = state_zones$fhsz_descr)
+
+# map zones by color to check import and conversion
+leaflet () %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolygons(data=state_zones,
+              fillColor=~colorpal(fhsz_descr),
+              fillOpacity=0.7,
+              color="white",
+              weight=1,
+              popup=~fhsz_descr)
+# checks out
+
+
