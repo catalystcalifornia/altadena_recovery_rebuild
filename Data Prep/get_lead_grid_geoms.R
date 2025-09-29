e2db59b7ab13f16c8c400f2b7fc69927d0cebe68
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-09-29 12:17:11 -0700

    adding lead testing results indicators and organizing the script. REPUSHED table to postgres and saved old table with _archive

--- Data Prep/get_lead_grid_geoms.R
+++ Data Prep/get_lead_grid_geoms.R
@@ -1,9 +1,17 @@
-# Get lead grid geoms from arcgis online REST API
+## Script adds grid geoms for lead testing data from LACDPH and joins attribute data to geoms
+## Sources: 
+# https://storymaps.arcgis.com/stories/667412ef37ee4392bddb5c90da3480f1#ref-n-5niwaH
+# https://www.arcgis.com/apps/mapviewer/index.html?url=https://services.arcgis.com/RmCCgQtiZLDCtblq/ArcGIS/rest/services/Eaton_Fire_Area_Reference_Grid_Roux_041725/FeatureServer/14/&source=sd 
+
+
+# Get lead grid geoms from arcgis online REST API ----
 
 library(httr) # GET()
 library(jsonlite) # content()
 library(sf) # st_read()
 library(mapview)
+library(readxl)
+library(stringr)
 
 source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("altadena_recovery_rebuild")
@@ -29,13 +37,68 @@ mapview(sf_object)
 # export to pg - rename columns, define schema, table names, etc.
 colnames(sf_object) <- c("grid_name", "geometry")
 
+
+
+# Get lead data (geometric mean and percent exceedances) for Eaton grids ----
+# We are only doing lead because per Rouxs summary paper -- lead was the only contaminant that exceeded acceptable thresholds post-fire
+# manually entered lead data
+# source: http://publichealth.lacounty.gov/media/wildfire/docs/Roux_Community_Soil_Sampling_and_Human_Health_Screening_Report_Eaton_and_Palisades_Fire_Regions.pdf
+# pg. 48 
+# manually entered here: W:\Project\RDA Team\Altadena Recovery and Rebuild\Data\LACDPH Roux Soil Testing\Eaton Results Lead Geometric Mean August 2025.xlsx
+geo_mean_df <- read_excel("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\LACDPH Roux Soil Testing\\Eaton Results Lead Geometric Mean August 2025.xlsx")
+
+
+# percentage exceedances from external appendix
+# source: https://storymaps.arcgis.com/stories/667412ef37ee4392bddb5c90da3480f1
+# downloaded a subfolder of Appendix B here: W:\Project\RDA Team\Altadena Recovery and Rebuild\Data\LACDPH Roux Soil Testing\Summary Statistics (By Grid), For Specific COCs
+pct_exceed_df <- read_excel("W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\LACDPH Roux Soil Testing\\Summary Statistics (By Grid), For Specific COCs\\Eaton_Lead_data_20250826.xlsx")
+
+# clean up column names
+colnames(pct_exceed_df) <- tolower(colnames(pct_exceed_df))
+
+pct_exceed_df <- pct_exceed_df %>%
+  rename(lead_samples_total=total_samples_lead,
+         lead_exceedances_count=total_exceedances_lead,
+         lead_exceedances_percent=percent_exceedances_lead)
+
+# clean up grid labels
+start_char <- "-"
+end_char <- "-"
+pattern <- paste0("(?<=\\", start_char, ").*?(?=\\", end_char, ")")
+pct_exceed_df$grid_name<- str_extract(pct_exceed_df$grid, pattern)
+View(pct_exceed_df)
+# F10 formatted differently, manually clean
+pct_exceed_df <- pct_exceed_df %>%
+  mutate(grid_name=case_when(is.na(grid_name) ~ "F10",
+                             TRUE ~ grid_name))
+
+View(pct_exceed_df)
+
+## Match two dataframes
+lead_data <- geo_mean_df %>% 
+              left_join(pct_exceed_df %>% select(-grid), by=c("grid"="grid_name"))
+
+View(lead_data) # looks good
+
+# Join geoms to lead data -------
+sf_object <- sf_object %>%
+  left_join(lead_data,by=c("grid_name"="grid"))
+
+sf_object <- st_transform(sf_object, 3310)
+
 schema <- "data"
 table_name <- "lacdph_lead_results_grid_2025"
-indicator <- "Grid geoms used to map lead testing for Eaton fire"
-source <- "LA County Department of Public Health"
+indicator <- "Lead testing results and grid geoms for mapping lead results post the Eaton fire. Soil sample testing only done for intact homes - property had less than 26% damage (no damage, minor, affected damage)"
+source <- "LA County Department of Public Health and Roux Associates
+Read more here: https://storymaps.arcgis.com/stories/667412ef37ee4392bddb5c90da3480f1?cover=false"
 qa_filepath <- "W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_lead_data.docx"
-column_names <- colnames(sf_object)[1]
-column_comments <- c("Grid label")
+column_names <- colnames(sf_object)
+column_names <- column_names[column_names !="geometry"]
+column_comments <- c("Grid label",
+                     "lead geometric mean for the grid measured in mg/kg, 80 or more indicates above residential soil screening levels considered protective over a lifetime of exposure",
+                     "total lead samples collected",
+                     "total lead samples above the residential soil screening level (80 mg/kg)",
+                     "percent of lead samples collected over the residential soil screening level")
 
 export_shpfile(con=con, df=sf_object, schema=schema, table_name=table_name, geometry_column = "geometry")
 
