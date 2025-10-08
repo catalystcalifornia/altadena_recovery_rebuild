be2b2476aa37205adb1c682fbc646a19595e079f
Jennifer Zhang <JZhang@catalystcalifornia.org>
2025-10-08 16:52:24 -0700

    adding more code to look at what AINs match and didn't match. also comment out mapview which is too slow and not running.

--- Data Prep/jan_sept_parcel_xwalk.R
+++ Data Prep/jan_sept_parcel_xwalk.R
@@ -30,7 +30,8 @@ parcels_jan <- st_read(con_alt, query="Select * from data.assessor_parcels_unive
 parcels_sept <- st_read(con_alt, query="Select * from data.assessor_parcels_universe_sept2025", geom="geom")%>%
   mutate(flag="sept")
 
-# get assessor data
+# get assessor data: not sure if we need this though so commenting out for now
+
 # assessor_jan <- st_read(con_alt, query="Select * from data.assessor_data_universe_jan2025")
 # assessor_sept <- st_read(con_alt, query="Select * from data.assessor_data_universe_sept2025")
 
@@ -53,7 +54,25 @@ names(parcels_join) <- gsub("\\.y$", "_sept", names(parcels_join))
 length(unique(parcels_join$ain_jan)) #54874
 length(unique(parcels_join$ain_sept)) #54874
 
-# See if any AINs are NA 
+# look at result just the AIN column
+join_ain<-parcels_join%>%select(ain_jan, ain_sept)
+
+# Pull out rows where teh AINs are matching
+same_ain <- parcels_join %>%
+  filter(!is.na(ain_jan) & ain_jan == ain_sept)%>%select(ain_jan, ain_sept)
+
+# See how many AINs is that:
+length(unique(same_ain$ain_sept)) #54825 
+
+# Now I want to extract the opposite: the rows where the jan ain and sept ain are not matching
+not_matching_ain <- parcels_join %>%
+  filter(!is.na(ain_jan) & !is.na(ain_sept) & ain_jan != ain_sept)%>%select(ain_jan, ain_sept)
+
+# See how many AINs is that:
+length(unique(not_matching_ain$ain_sept)) #54813 
+
+
+# See if any AINs are NA after the join 
 
 sept_not_joined <- parcels_join %>%
   filter(is.na(ain_sept))%>%View() # None
