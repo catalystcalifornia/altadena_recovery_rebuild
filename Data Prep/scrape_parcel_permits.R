811370fad97a8e0d0b5a0eb02f56a832327bc369
Hillary Khan <HKhan@catalystcalifornia.org>
2025-10-11 09:59:03 -0700

    clean up more

--- Data Prep/scrape_parcel_permits.R
+++ Data Prep/scrape_parcel_permits.R
@@ -9,16 +9,10 @@ source("Data Prep\\scraping_functions.R")
 
 con <- connect_to_db("altadena_recovery_rebuild")
 
-##### Test dataframes: use to confirm we're getting correct data responses before applying to full parcel universe #####
-# Test addresses (1 LA County, 1 Pasadena)
-unincorporated_parcels <- data.frame(address=c("2204 Grand Oaks Avenue", "1630 Carriage House Rd"),
-                                     expected_permits=c(5, 0)) 
-
 # EPIC LA - LA County building permits (unincorporated cities only)
 lac_permits_url <- "https://epicla.lacounty.gov/energov_prod/SelfService/#/search?m=2&ps=100&pn=1&em=true&st="
 
 
-##### January Universe #####
 jan_parcels <- dbGetQuery(con, 
                           "SELECT DISTINCT dmgs.ain, dmgs.damage_category, xwalk.site_address_parcel FROM data.rel_assessor_damage_level as dmgs LEFT JOIN data.crosswalk_dins_assessor_jan2025 as xwalk ON dmgs.ain = xwalk.ain WHERE dmgs.damage_category = 'Significant Damage' OR dmgs.damage_category = 'Some Damage' ORDER BY dmgs.ain")
 
@@ -29,13 +23,11 @@ test_chromote()
 # 2. If above works, try to extract data from one test url to get general data fields
 url_ <- "https://epicla.lacounty.gov/energov_prod/SelfService/#/search?m=2&ps=10&pn=1&em=true&st=2204%20Grand%20Oaks%20Avenue"
 message(paste("Current search URL:", url_))
-page_response <- wait_for_spa_load(url=url_)
-permits <- extract_permit_data_general(url=url_, html_content=page_response)  # Extract the data
-Sys.sleep(5)
+permits <- scrape_permits_chromote(url=url_, wait_time = 30)
 
 # 3. If above works, scale it up to loop through a list of addresses and return all permits (with general data fields)
 # using 10 Jan parcels (Altadena Only)
-test <- jan_parcels %>%
+altadena_parcels <- jan_parcels %>%
   mutate(city = case_when(grepl("ALTADENA, CA", site_address_parcel)~"Altadena",
                           grepl("PASADENA, CA", site_address_parcel)~"Pasadena",
                           .default="something else!")) %>%
@@ -45,15 +37,15 @@ test <- jan_parcels %>%
 
 final_data <- NULL
 
-for (row_ in 5277:nrow(test)) { # updated from 1701 to 5277; # updated from 1 to 1701 to pick up from error (noted below)
+for (row_ in 1:nrow(altadena_parcels)) { 
   
-  address_url <- test[row_, "portal_url"]
+  address_url <- altadena_parcels[row_, "portal_url"]
   message(paste(row_, ":", address_url))
   result <- scrape_permits_chromote(url=address_url, wait_time = 30)
   result <- result %>%
-    mutate(ain=test[row_, "ain"],
-           site_address_parcel=test[row_, "site_address_parcel"],
-           damage_category=test[row_, "damage_category"])
+    mutate(ain=altadena_parcels[row_, "ain"],
+           site_address_parcel=altadena_parcels[row_, "site_address_parcel"],
+           damage_category=altadena_parcels[row_, "damage_category"])
   
   if(is.null(final_data)) {
         final_data <- result
@@ -69,7 +61,7 @@ for (row_ in 5277:nrow(test)) { # updated from 1701 to 5277; # updated from 1 to
 #              overwrite = FALSE, row.names = FALSE) 
 # 
 # dbSendQuery(con, "COMMENT ON TABLE data.general_permit_data_10_2025 IS
-#             'Damage Inspection data for the Eaton Fire from CAL FIRE eGIS, June 2, 2025 in SRID 3310
+#             'General permit data for Altadena parcels with some or significant damage,
 #             Data imported on 10-12-25
 #             QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_scrape_permit_data.docx
 #             Source: https://epicla.lacounty.gov/energov_prod/SelfService/#/search?m=2&ps=100&pn=1&em=true&st=[ain]'")
@@ -79,7 +71,7 @@ for (row_ in 5277:nrow(test)) { # updated from 1701 to 5277; # updated from 1 to
 # wait_for_spa_load(): need a way to know how many total permits (results) there are and if we need to repeat scrape for subsequent pages 
 ## - for now we assume all parcels have 100 permits or fewer - can see if any have 100 permits and then look to see if there are more
 
-# if a ain is associated with a timeout/error status, we'll need to add a section to this script that perhaps reruns them with a
+# if an ain is associated with a timeout/error status, we'll need to add a section to this script that perhaps reruns them with a
 # longer max_wait time.
 
 # will want to put this on a lambda/aws schedule to run monthly without worrying about RDP staying connected
