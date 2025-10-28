c34357ebf5a7b6415ddd2415189e6440ab4afc5e
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-10-27 22:47:46 -0700

    done with fire hazard zones

--- Analysis/analysis_fire_hazards.R
+++ Analysis/analysis_fire_hazards.R
@@ -18,10 +18,10 @@ options(scipen = 999)
 #### Step 1: Pull in relational datasets ####
 housing_sept <- st_read(con, query="SELECT * FROM data.rel_assessor_residential_sept2025 WHERE residential = 'true'") 
 
-damage <- st_read(con, query="SELECT * FROM data.rel_assessor_damage_level_sept2025") %>%
-  filter(damage_category=="Significant Damaged")
+damage <- st_read(con, query="SELECT * FROM data.rel_assessor_damage_level_sept2025")
 
-fhsz_local <- st_read(con, query = "Select * FROM data.local_fhsz_3310", geom="geom")
+fhsz_local <- st_read(con, query = "Select * FROM data.local_fhsz_3310", geom="geom") %>%
+  filter(fhsz_descr!="NonWildland")
 
 fhsz_state <- st_read(con, query = "Select * FROM data.state_fhsz_3310", geom="geom")
 
@@ -37,191 +37,174 @@ st_crs(parcels_sept)
 parcels_sept <- parcels_sept %>%
   filter(ain_sept %in% housing_sept$ain_sept)
 
-# select damaged parcels only
-parcels_sept_damage <- parcels_sept %>%
-  filter(ain_sept %in% damage$ain_sept)
+parcels_local_fhsz<- st_intersection(parcels_sept,fhsz_local)
 
-# join parcels to zones based on centroid
-parcels_zoning <- st_join(st_centroid(parcels_sept_damage), zoning_lac, left=TRUE)
+# #check
+# mapview(parcels_local_fhsz) # looks good
+# nrow(parcels_local_fhsz)
+# length(unique(parcels_local_fhsz$ain_sept))
+# duplicates, keep the highest hazard
 
-# check join
-table(parcels_zoning$z_category, useNA='always')
-# no NAs
+parcels_local_fhsz_dedup <-parcels_local_fhsz %>%
+  st_drop_geometry() %>%
+  group_by(area_name,ain_sept) %>%
+  summarize(count=n(),
+    local_fhsz_list=paste(fhsz_descr, collapse = ", ")) %>%
+ ungroup()
 
-nrow(parcels_zoning)
-length(unique(parcels_zoning$ain_sept))
-# no duplicates
 
-### Step 3: Explore zoning #####
-# compare how zoning in Assessor data matches up to our join/data
-parcels_zoning <- parcels_zoning %>%
-  left_join(housing_sept %>% select(ain_sept, use_code, zoning_code), by="ain_sept")
+parcels_local_fhsz_dedup <-  parcels_local_fhsz_dedup %>%
+  mutate(local_fhsz=
+           case_when(grepl("Very High",local_fhsz_list) ~ "Very High",
+                     grepl("High",local_fhsz_list) ~ "High",
+                     grepl("Moderate",local_fhsz_list) ~ "Moderate",
+                     TRUE ~NA))
 
-check <- parcels_zoning %>%
-  group_by(zoning_code,zone) %>%
+check <- parcels_local_fhsz_dedup %>%
+  group_by(local_fhsz, local_fhsz_list) %>%
   summarise(count=n())
 
-View(check)
-# some discrepancies in intensity, let's use our zoning data, which doesn't have blanks either
 
-# do an initial summary to determine which field to use and we can recode from there
-zone_table1 <- parcels_zoning %>%
-  group_by(zone) %>%
-  summarise(property_count=n())
+#### Step 3: Join parcel data to state fire hazard zones data ####
+st_crs(fhsz_state)
+st_crs(parcels_sept)
+# right projections
 
-zone_cat_table1 <- parcels_zoning %>%
-  group_by(z_category) %>%
-  summarise(property_count=n())
-# mostly all R1 the zone provides further detail about intensity than z_category
-# so first do zone category then drill down into R1
+parcels_state_fhsz<- st_intersection(parcels_sept,fhsz_state)
 
-### Step 4: Calculate the potential uses by area #####
-# First the percentage of damaged properties in each area with their overall zoning categories
-# drop geometry 
-all_df <- parcels_zoning %>%
-  st_drop_geometry()
+# #check
+# mapview(parcels_state_fhsz) # looks good
+nrow(parcels_state_fhsz)
+length(unique(parcels_state_fhsz$ain_sept))
+# no duplicates
+table(parcels_state_fhsz$fhsz_descr,useNA='always')
 
-analysis_zone_category_e_w <- all_df %>%
-  group_by(area_name) %>% 
-  mutate(total=n()) %>%
-  ungroup() %>%
-  group_by(area_name,z_category) %>%
-  summarise(count=n(),
-            prc=n()/min(total)*100,
-            total=min(total))
+parcels_state_fhsz <- parcels_state_fhsz %>%
+  st_drop_geometry() %>%
+  select(area_name,ain_sept,fhsz_descr) %>%
+  rename(state_fhsz=fhsz_descr)
 
-analysis_zone_category_alt <- all_df %>%
-  mutate(area_name='Altadena') %>%
-  group_by(area_name) %>% 
-  mutate(total=n()) %>%
-  ungroup() %>%
-  group_by(area_name,z_category) %>%
-  summarise(count=n(),
-            prc=n()/min(total)*100,
-            total=min(total))
+#### Step 4: Join together the local and state fhsz joins ####
+parcels_fhsz <- parcels_local_fhsz_dedup %>% select(-count) %>% ungroup() %>%
+  full_join(parcels_state_fhsz, by=c("ain_sept","area_name"))
 
-analysis_z_category <- rbind(analysis_zone_category_e_w,
-                             analysis_zone_category_alt)
+# check
+table(parcels_fhsz$local_fhsz,useNA='always')
+table(parcels_local_fhsz_dedup$local_fhsz,useNA='always')
+
+table(parcels_fhsz$state_fhsz,useNA='always')
+table(parcels_state_fhsz$state_fhsz,useNA='always')
+
+nrow(parcels_fhsz)
+length(unique(parcels_fhsz$ain_sept))
+# no duplicates
+
+View(parcels_fhsz)
+
+# create a unified column
+check <- parcels_fhsz %>%
+  group_by(local_fhsz, state_fhsz) %>%
+  summarise(count=n())
+
+parcels_fhsz <- parcels_fhsz %>%
+  mutate(combined_fhsz=
+           case_when(local_fhsz=="Very High" & state_fhsz=="Very High" ~ "Very High",
+                     is.na(local_fhsz) & state_fhsz=="Very High" ~ "Very High",
+                     !is.na(local_fhsz) & is.na(state_fhsz) ~ local_fhsz,
+                     TRUE ~ NA))
+
+View(parcels_fhsz)
 
 
-# recode uses
-unique(analysis_z_category$z_category)
-# based on https://planning.lacounty.gov/wp-content/uploads/2024/04/WSGVAP_LargeMap_Zoning_240330_Altadena.pdf
-# https://planning.lacounty.gov/wp-content/uploads/2022/04/map_z_02_Altadena.pdf
-# R-1-7500
-# C-2: Neighborhood Business
-# C-3: General Commercial
-# R-1: Single-Family Residence
-# R-2: Two-Family Residence
-# R-3-()U: Limited Density Multiple Residence
-# R-A: Residential Agricultural
-# A-1: Light Agriculture
-# O-S: Open Space
-# SP: Specific Plan
+#### Step 5: Clean fire hazard join and push to postgres as relational table ####
+# add missing parcels that didn't match to any fire hazard
+# create a dataframe with no hazard for the parcels that didn't match to a hazard zone
+parcels_fhsz_none <- parcels_sept %>%
+  filter(!ain_sept %in% parcels_fhsz$ain_sept) %>%
+  st_drop_geometry() %>%
+  select(area_name, ain_sept) %>%
+  mutate(local_fhsz_list=NA,
+         local_fhsz="None",
+         state_fhsz="None",
+         combined_fhsz="None")
 
-zone_cat_table1 <- zone_cat_table1 %>%
-  mutate(z_category_label=
-           case_when(
-             z_category=="C-2" ~ "Neighborhood Business",
-             z_category=="C-3" ~ "General Commercial",
-             z_category=="R-1" ~ "Single-Family Residence",
-             z_category=="R-2" ~ "Two-Family Residence",
-             z_category=="R-3-()U" ~ "Limited Density Multiple Residence",
-             z_category=="R-A" ~ "Residential Agricultural",
-             z_category=="A-1" ~ "Light Agriculture",
-             z_category=="O-S" ~ "Open Space",
-             z_category=="SP" ~ "Specific Plan",
-             TRUE ~ NA
-           )) %>%
-  st_drop_geometry()
+# combine dataframes together
+parcels_fhsz_final <- rbind(parcels_fhsz_none,parcels_fhsz)
 
-# join labels
-analysis_z_category <- analysis_z_category  %>%
-  left_join(zone_cat_table1 %>% select(z_category,z_category_label)) 
-
-# clean up for postgres 
-analysis_z_category_final <- analysis_z_category %>%
-  select(area_name, z_category, z_category_label, count, prc, total) %>%
-  rename(zone_count=count,
-         zone_prc=prc,
-         area_total=total) %>% ungroup()
-
-# Upload tables to postgres and add table/column comments
-# dbWriteTable(con, name = "analysis_zone_categories_sept2025", value = analysis_z_category_final, overwrite = FALSE)
+# Upload table to postgres as relational table and add table/column comments
+# dbWriteTable(con, name = "rel_assessor_fhsz_sept2025", value = parcels_fhsz_final, overwrite = FALSE)
 # schema <- "data"
-# table_name <- "analysis_zone_categories_sept2025"
-# indicator <- "Allowable zone categories for significantly damaged properties in each area, e.g., % of significantly damaged properties in West Altadena with a R-1 (single family residence) allowable use"
-# source <- "Source: LA County Assessor Data, September 2025 & LA County plannning data"
+# table_name <- "rel_assessor_fhsz_sept2025"
+# indicator <- "Relational table of september residential parcels that include the local and state responsibility fire hazard zones. If the parcels is not in a fire hazard zone then the category None is used"
+# source <- "Source: LA County Assessor Data, September 2025 & Local & state fire hazard zones from Cal office of fire marshall"
 # qa_filepath <- " QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_analysis_zones.docx"
-# column_names <- colnames(analysis_z_category_final) # Get column names
+# column_names <- colnames(parcels_fhsz_final) # Get column names
 # column_names
 # column_comments <- c(
 #   "area name",
-#   "zoning category short label",
-#   "zoning category long label",
-#   "count of properties in the area within the zone",
-#   "prc of properties in the area within the zone",
-#   "total properties in the zone")
+#   "ain september 2025",
+#   "list of local fire hazard zones the parcel fell within",
+#   "local fire hazard zone - top coded based on most severe zone--uses this or combined_fhsz",
+#   "main local fire hazard zone - top coded for most severe zone parcel falls within",
+#   "state fire hazard zone",
+#   "combined category of fire hazard zone accounting for state and local zones")
 # add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
 
-### Step 5: Calculate the zones within R-1 #####
-# The percentage of damaged properties within R-1 by lot size minimums
-# can't find specific information on the zones, but I believe they are based on what the minimum lot size should be
-# other municipality in LA County https://codelibrary.amlegal.com/codes/baldwinpark/latest/baldwinpark_ca/0-0-0-9435
-# https://altadenatowncouncil.org/documents/Altadena_CSD.pdf
-# each lot size has it's own requirements
-analysis_zone_e_w <- all_df %>%
-  filter(z_category=='R-1') %>% # keep just R-1
-  group_by(area_name) %>% 
+
+### Step 6: Calculate the fire hazard zones prevalence by damage level for west and east altadena #####
+# what percentage of properties are within the fire hazard zones by damage level
+# bring in relational table
+all_df <- st_read(con, query="SELECT * FROM data.rel_assessor_fhsz_sept2025") %>%
+  left_join(damage)
+
+analysis_fhsz_e_w <- all_df %>%
+  group_by(area_name,damage_category) %>% 
   mutate(total=n()) %>%
   ungroup() %>%
-  group_by(area_name,zone) %>%
+  group_by(area_name,damage_category,combined_fhsz) %>%
   summarise(count=n(),
             prc=n()/min(total)*100,
             total=min(total))
 
-analysis_zone_alt <- all_df %>%
+analysis_fhsz_alt <- all_df %>%
   mutate(area_name='Altadena') %>%
-  filter(z_category=='R-1') %>% # keep just R-1
-  group_by(area_name) %>% 
+  group_by(area_name,damage_category) %>% 
   mutate(total=n()) %>%
   ungroup() %>%
-  group_by(area_name,zone) %>%
+  group_by(area_name,damage_category,combined_fhsz) %>%
   summarise(count=n(),
             prc=n()/min(total)*100,
             total=min(total))
 
-analysis_zone <- rbind(analysis_zone_e_w,
-                       analysis_zone_alt)
+analysis_fhsz <- rbind(analysis_fhsz_e_w,
+                             analysis_fhsz_alt) %>%
+  rename(area_damage_total=total,
+         fhsz_count=count,
+         fhsz_prc=prc) %>%
+  ungroup()
 
-
-# recode limits
-unique(analysis_zone$zone)
-# don't recode just specify in table comments and writeup
-
-# clean up for postgres 
-analysis_zone_final <- analysis_zone %>%
-  select(area_name, zone, count, prc, total) %>%
-  rename(zone_count=count,
-         zone_prc=prc,
-         area_total=total) %>% ungroup()
+check <- analysis_fhsz %>%
+  group_by(area_name,damage_category) %>%
+  summarise(sum=sum(fhsz_prc))
+# checks out
 
 # Upload tables to postgres and add table/column comments
-# dbWriteTable(con, name = "analysis_zone_r1_sept2025", value = analysis_zone_final, overwrite = FALSE)
-# schema <- "data"
-# table_name <- "analysis_zone_r1_sept2025"
-# indicator <- "Allowable zone categories and lot sizes for significantly damaged properties in R1 (single family) zones for each area, e.g., % of significantly damaged R-1 properties in West Altadena with a R-1 minimum 7500 lot size use"
-# source <- "Source: LA County Assessor Data, September 2025 & LA County plannning data"
-# qa_filepath <- " QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_analysis_zones.docx"
-# column_names <- colnames(analysis_zone_final) # Get column names
-# column_names
-# column_comments <- c(
-#   "area name",
-#   "zone with specific lot size for R-1",
-#   "count of properties in the area within the zone",
-#   "prc of properties in the area within the zone",
-#   "total properties in the zone")
-# add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
+dbWriteTable(con, name = "analysis_damage_fhsz_sept2025", value = analysis_fhsz, overwrite = FALSE)
+schema <- "data"
+table_name <- "analysis_damage_fhsz_sept2025"
+indicator <- "Analysis of the percentage of properties in West and East Altadena in fire hazard areas by damage category, e.g., the % of significantly damaged properties in very high fire hazard areas in West Altadena"
+source <- "Source: LA County Assessor Data, September 2025 & California Office of Fire Marshall state and local responsibility areas"
+qa_filepath <- " QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_analysis_zones.docx"
+column_names <- colnames(analysis_fhsz) # Get column names
+column_names
+column_comments <- c(
+  "area name",
+  "damage category",
+  "combined local and state fire hazard zone - top coded",
+  "count of properties in the zone within the area-damage category - numerator",
+  "percent of properties in the zone out of all properties in the area-damage category",
+  "total properties in the combined area-damage category - denominator")
+add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
 
-#### Step 8: close connection ####
+#### Step 7: close connection ####
 dbDisconnect(con)
