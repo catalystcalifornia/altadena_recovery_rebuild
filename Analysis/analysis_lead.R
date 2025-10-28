4e273d0d4b0604b2540a7c56703030a3b77e64a5
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-10-28 15:29:35 -0700

    updating join table and pushing to postgres

--- Analysis/analysis_lead.R
+++ Analysis/analysis_lead.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(purrr)
 library(stringr)
 library(RPostgres)
+library(mapview)
 
 #Add database connection and source script with functions for pushing to postgres
 source("W:\\RDA Team\\R\\credentials_source.R")
@@ -15,7 +16,7 @@ options(scipen = 999)
 
 
 #### Step 1: Pull in relational datasets ####
-parcels <- st_read(con, query="SELECT * FROM data.rel_assessor_altadena_parcels_jan2025", geom = "geom") 
+parcels <- st_read(con, query="SELECT * FROM data.rel_assessor_altadena_parcels_sept2025", geom = "geom") 
 
 hi_lead <- st_read(con, query="SELECT * FROM data.lacdph_lead_results_grid_2025", geom = "geom") %>% 
   filter(lead_geometric_mean > 80) 
@@ -24,6 +25,9 @@ lead <- st_read(con, query="SELECT * FROM data.lacdph_lead_results_grid_2025", g
   mutate(hi_lead_flag = ifelse(lead_geometric_mean > 80, TRUE, FALSE)) 
 
 #### Step 2: intersect and compute which grid has the majority of the parcel in it, assign to that one ####
+st_crs(lead)
+st_crs(parcels)
+# mapview(lead) + mapview(parcels)
 
 ain_hi_lead <- st_intersection(parcels %>%
                           mutate(ain_id = row_number()),
@@ -54,22 +58,70 @@ ain_lead <- st_intersection(parcels %>%
                             st_drop_geometry() %>%
                             select(-c(gid, gid.1))
 
+# clean up table and for parcels missing from grids add flag for not assessed
+ain_lead_all <- ain_lead %>%
+  select(ain_sept, area_name, area_label, grid_name, lead_geometric_mean, hi_lead_flag) %>%
+  mutate(hi_lead_label=case_when(
+    hi_lead_flag==TRUE ~ "High Lead Grid",
+    TRUE ~ "Not High Lead Grid"
+  ))
+
+ain_lead_missing <- parcels %>%
+  filter(!ain_sept %in% ain_lead_all$ain_sept) %>%
+  select(ain_sept, area_name, area_label) %>%
+  mutate(grid_name=NA,
+         lead_geometric_mean=NA,
+         hi_lead_flag=NA,
+         hi_lead_label="Not Tested") %>%
+  st_drop_geometry()
+
+ain_lead_all <- rbind(ain_lead_missing, ain_lead_all)
+nrow(ain_lead_all)
+nrow(parcels)
+length(unique(ain_lead_all$ain_sept))
+duplicate_rows <- ain_lead_all[(duplicated(ain_lead_all$ain_sept, fromLast = FALSE) | duplicated(ain_lead_all$ain_sept, fromLast = TRUE)), ]
+# keep one
+
+ain_lead_all <- distinct(ain_lead_all)
+table(ain_lead_all$hi_lead_label,useNA='always')
+
+ain_lead_final_df <- ain_lead_all %>%
+  select(-area_name, -area_label)
+
+# export to postgres
+# dbWriteTable(con, name = "rel_assessor_lead_sept2025", value = ain_lead_final_df, overwrite = FALSE)
+# schema <- "data"
+# table_name <- "rel_assessor_lead_sept2025"
+# indicator <- "September 2025 assessor parcels matched to high lead grids. Includes all parcels not just residential. Includes flag for if lead geometric mean in test grid was over the recommended lifetime exposure of 80"
+# source <- "Multiple Data Sources"
+# qa_filepath <- " QA DOC: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Documentation\\QA_Sheet_analysis_lead.docx"
+# column_names <- colnames(ain_lead_final_df) # Get column names
+# column_names
+# column_comments <- c(
+#   "AIN for september",
+#   "lead testing grid",
+#   "lead geometric mean for testing grid, NA if not tested",
+#   "True-false for if testing grid mead was above 80, NA if not tested",
+#   "Label for if area is in high lead area or not, or if not tested"
+# )
+# add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
+
+
 #### Step 3: combine with damage and residential type database ####
-damage_df <- st_read(con, query="SELECT * FROM data.rel_assessor_damage_level")
+damage_df <- st_read(con, query="SELECT * FROM data.rel_assessor_damage_level_sept2025")
   
-restype_df <- st_read(con, query="SELECT * FROM data.rel_assessor_residential_jan2025_recode")
+restype_df <- st_read(con, query="SELECT * FROM data.rel_assessor_residential_sept2025")%>%
+  filter(residential==TRUE) # just keep residential properties
 
-lead_damage_df <- ain_hi_lead %>%
-  left_join(damage_df, by = "ain")
+lead_damage_df_all <- ain_lead_all  %>%
+  left_join(damage_df, by = "ain_sept")
 
-lead_damage_restype_df <- lead_damage_df %>%
-  left_join(restype_df, by = "ain")
-
-lead_damage_df_all <- ain_lead %>%
-  left_join(damage_df, by = "ain")
-
+# final df just for residential properties
 lead_damage_restype_df_all <- lead_damage_df_all %>%
-  left_join(restype_df, by = "ain")
+  right_join(restype_df, by = "ain_sept")
+
+# check
+nrow(restype_df)
 
 #### Step 4: calculate by Altadena, West, East for res_type and for owner_renter ####
 #calculate # of total 
