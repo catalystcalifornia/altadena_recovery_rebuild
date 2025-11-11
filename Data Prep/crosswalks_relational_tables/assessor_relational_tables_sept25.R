299a26bec20974c0f9f44784a5da5d782524ba7c
Elycia Mulholland Graves <egraves@advanceproj.org>
2025-11-10 21:40:41 -0800

    revisited sales tables and looked at nulls, recoded them to false based on exploration, repushed relational sales table and saved old one as _archive

--- Data Prep/crosswalks_relational_tables/assessor_relational_tables_sept25.R
+++ Data Prep/crosswalks_relational_tables/assessor_relational_tables_sept25.R
@@ -617,11 +617,13 @@ sales <- rel_res_df %>%
          tax_stat_key,
          impairment_key,
          partial_interest,
-         exemption_type
+         exemption_type,
+         ownership_code
          ) 
 
 sales <- sales %>%
-  mutate(last_sale_char=as.character(last_sale_date),
+  mutate(last_sale_date_orig=last_sale_date,
+    last_sale_char=as.character(last_sale_date),
     last_sale_date=as.Date(last_sale_char, format = "%Y%m%d"),
     sale_two_char=as.character(sale_two_date),
     sale_two_date=as.Date(sale_two_char, format = "%Y%m%d"),
@@ -664,11 +666,32 @@ sales <- sales %>%
   mutate(sold_after_eaton=ifelse(last_sale_date>="2025-02-08", TRUE, FALSE))
 
 # quick check
-
 sales%>%select(last_sale_date, sold_after_eaton)%>%View() # Looks good
+
+# check for nulls
+check_sales <- sales %>%
+  group_by(last_sale_year,sold_after_eaton) %>%
+  summarise(count=n())
+# 78 NA check
+na_sale_date <- sales %>%
+  select(last_sale_date_orig, last_sale_year,recording_date, doc_reason_code, land_reason_key, everything()) %>%
+  filter(is.na(sold_after_eaton))
+# those with a sales date originally have errors in the sales date, but sold prior to 2025
+# should adjust in regular update code to manually cap sale date to a date in the month
+# looking at recording date, only one had a recording date in 2025, but the recording was due to perfection of title not a sale, make nulls a FALSE flag
+# land reason key 6 for this property but key 6 isn't in data dictionary
+
 
 # clean up for postgres
 sales_final <- sales %>%
+  mutate(sold_after_eaton=ifelse(is.na(sold_after_eaton), FALSE, sold_after_eaton))
+
+# check
+table(sales$sold_after_eaton,useNA='always')
+table(sales_final$sold_after_eaton,useNA='always')
+# looks good
+
+sales_final <- sales_final %>%
   select(ain,
          sold_after_eaton, last_sale_year, last_sale_month, 
          first_owner_name, first_owner_name_overflow, second_owner_name,
@@ -691,33 +714,33 @@ View(sales_final)
 # 
 # dbWriteTable(con_alt, Id(schema, table_name), sales_final,
 #              overwrite = FALSE, row.names = FALSE)
-
+# 
 # # Add metadata
 # column_names <- colnames(sales_final) # Get column names
 # column_names
 # column_comments <- c('ain for september use to match to other tables',
-#         ' true false field for if sale took place after 2-8-25--likely to have been listed after eaton fire', 
-#          'year of last sale', 
-#          'month of last sale in number format', 
-#          'owner name', 
-#          "owner name overflow", 
+#         ' true false field for if sale took place after 2-8-25--likely to have been listed after eaton fire',
+#          'year of last sale',
+#          'month of last sale in number format',
+#          'owner name',
+#          "owner name overflow",
 #          "second owner name",
-#          " This is the date of last change or correction of ownership", 
-#          "This element contains a code that describes the relationships between the recording and valuation dates", 
-#          "This element contains a one digit code which identifies the specific reason for a reappraisable or  non reappraisable status", 
-#         "Reason key for the last land value change", 
+#          " This is the date of last change or correction of ownership",
+#          "This element contains a code that describes the relationships between the recording and valuation dates",
+#          "This element contains a one digit code which identifies the specific reason for a reappraisable or  non reappraisable status",
+#         "Reason key for the last land value change",
 #          "The percentage of property involved in a transfer of ownership. First two digits are percentage of property being transferred rounded to nearest whole. Third digit indicates the specific interest being transferred",
-#          "A one-digit code that indicates whether or not property taxes are delinquent", 
-#          "If parcel is delinquent, this indicates the four digits of the year in which taxes first became delinquent", 
+#          "A one-digit code that indicates whether or not property taxes are delinquent",
+#          "If parcel is delinquent, this indicates the four digits of the year in which taxes first became delinquent",
 #         "A key indicating whether the parcel value has been impaired and describing the impairment",
-#          "This is the last sale date - Present for both verified and unverified sales", 
-#          "Verification key of last sale - only unverified sales appear on the Secured Basic File Abstract", 
+#          "This is the last sale date - Present for both verified and unverified sales",
+#          "Verification key of last sale - only unverified sales appear on the Secured Basic File Abstract",
 #          "Last unverified sale amount - If the sale is a verified sale (non-numeric character as indicated on the verifications key), the sale amount will not show",
-#         "This is the second to last sale date - Present for both verified and unverified sales", 
-#         "Verification key of second to last sale - Only unverified sales appear on the Secured Basic File Abstract", 
+#         "This is the second to last sale date - Present for both verified and unverified sales",
+#         "Verification key of second to last sale - Only unverified sales appear on the Secured Basic File Abstract",
 #         "Second to Last unverified sale amount - If the sale is a verified sale (non-numeric character as indicated on the verifications key), the sale amount will not show",
-#         "This is the third to last sale date - Present for both verified and unverified sales", 
-#         "Verification key of third to last sale - Only unverified sales appear on the Secured Basic File Abstract", 
+#         "This is the third to last sale date - Present for both verified and unverified sales",
+#         "Verification key of third to last sale - Only unverified sales appear on the Secured Basic File Abstract",
 #         "Third to last unverified sale amount - If the sale is a verified sale (non-numeric character as indicated on the verifications key), the sale amount will not show")
 # 
 # add_table_comments(con_alt, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
