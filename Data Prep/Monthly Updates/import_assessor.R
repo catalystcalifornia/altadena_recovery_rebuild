5c7511b976af7a06f93931242aade52942d711ca
Hillary Khan <hkhan@catalystcalifornia.org>
2026-08-03 12:31:34 -0700

    review QA steps and re-export table with updated AINs

--- Data Prep/Monthly Updates/import_assessor.R
+++ Data Prep/Monthly Updates/import_assessor.R
@@ -229,7 +229,7 @@ dbSendQuery(con, sql_recode)
 
 ## REVIEW: Two kinds of AIN mismatches between CSV and SHP files ##
 # For permit scraping we are primarily interested in mismatches where the associated 
-# use code starts with zero (residential) and does NOT end with V (vacant lot)
+# use code starts with zero (residential) and DOES NOT end with V (vacant lot)
 
 shp_ain <- shp_ain_universe
 csv_ain <- dbGetQuery(con, statement=paste0("SELECT * FROM ", schema, ".", csv_table_name, ";"))
@@ -241,8 +241,19 @@ mismatch_1 <- data.frame(ain=setdiff(shp_ain, csv_ain$ain)) %>%
            crs=st_crs(shp_intersect)) 
 
 # number of mismatch 1
-nrow(mismatch_1) # 12
+nrow(mismatch_1) # 16
 sort(mismatch_1$ain)
+
+# "5823022022" "5823022023" 
+# "5827014035" "5827014036" 
+# "5830016058" 
+# "5831016035" "5831016036"
+# "5839016025" "5839016026" 
+# "5841007024" 
+# "5842008017" "5842008018" 
+# "5843023069" "5843023070"
+# "5844011016" 
+# "5863003900"
 
 # visually confirm shapes are fully in Altadena
 mapview(city_perimeters, col.regions="lightgrey") + mapview(mismatch_1, col.regions="red")
@@ -252,19 +263,23 @@ mapview(city_perimeters, col.regions="lightgrey") + mapview(mismatch_1, col.regi
 # prioritize residential and non-vacant if updating script
 
 ## No responses - should figure these out
+# "5823022022" - NEW! - https://portal.assessor.lacounty.gov/parceldetail/5823022022
+# "5823022023" - NEW! - https://portal.assessor.lacounty.gov/parceldetail/5823022023
+# "5830016058" - NEW! - https://portal.assessor.lacounty.gov/parceldetail/5830016058 <-- matched calfire; looks like it should be 5830016052 - https://portal.assessor.lacounty.gov/parceldetail/5830016052
 # "5831016035" - no response (same) - https://portal.assessor.lacounty.gov/parceldetail/5831016035 <-- matched calfire
 # "5831016036" - no response (same) - https://portal.assessor.lacounty.gov/parceldetail/5831016036 <-- matched calfire
 # "5839016026" - no response (same) - https://portal.assessor.lacounty.gov/parceldetail/5839016026 <-- leftover / mapsearch shows same AIN 
-# (NEW) "5841007024" - no response - https://portal.assessor.lacounty.gov/parceldetail/5841007024 <-- matched calfire
+# "5841007024" - no response (same) - https://portal.assessor.lacounty.gov/parceldetail/5841007024 <-- matched calfire
 # "5843023070" - no response (same) - https://portal.assessor.lacounty.gov/parceldetail/5843023070 <-- matched calfire; looks like it should be 5843023037 - https://portal.assessor.lacounty.gov/parceldetail/5843023037
+# "5844011016" - NEW! - https://portal.assessor.lacounty.gov/parceldetail/5844011016 <-- matched calfire; looks like it should be 5844011003 - https://portal.assessor.lacounty.gov/parceldetail/5844011003
 
 ## Shells - not sure we can do anything with these - no associated addresses
-# "5827014035" - 0100/shell - https://portal.assessor.lacounty.gov/parceldetail/5827014035 <-- matched calfire - no damage 
-# "5827014036" - 0100/shell - https://portal.assessor.lacounty.gov/parceldetail/5827014036 <-- leftover / mapsearch shows same AIN (looks to be part of 5827014035) 
+# "5827014035" - 0100/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5827014035 <-- matched calfire - no damage 
+# "5827014036" - 0100/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5827014036 <-- leftover / mapsearch shows same AIN (looks to be part of 5827014035) 
 # "5839016025" - 0100/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5839016025 <-- leftover / mapsearch shows same AIN 
-# "5842008017" - 0100/shell - https://portal.assessor.lacounty.gov/parceldetail/5842008017 <-- leftover / mapsearch shows same AIN (looks to be part of 5842008018) 
-# "5842008018" - 0100/shell - https://portal.assessor.lacounty.gov/parceldetail/5842008018 <-- matched calfire - destroyed residence
-# "5843023069" - 0101/shell (updated; has response) - https://portal.assessor.lacounty.gov/parceldetail/5843023069 <- matched calfire
+# "5842008017" - 0100/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5842008017 <-- leftover / mapsearch shows same AIN (looks to be part of 5842008018) 
+# "5842008018" - 0100/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5842008018 <-- matched calfire - destroyed residence
+# "5843023069" - 0101/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5843023069 <- matched calfire
 # "5863003900" - 010v/shell (same) - https://portal.assessor.lacounty.gov/parceldetail/5863003900 <- leftover/mapsearch shows same AIN
 
 # Previous mismatches (no longer show up in mismatch_1):
@@ -278,11 +293,12 @@ calfire <- st_read(con, query="SELECT din_id, apn_parcel, damage, street_number,
 calfire_matches <- st_intersection(mismatch_1, calfire) %>%
   select(ain, apn_parcel, damage, everything())
 
-matched <-  mismatch_1 %>% filter(ain %in% unique(calfire_matches$ain)) # 7
+matched <-  mismatch_1 %>% filter(ain %in% unique(calfire_matches$ain)) # 9
 sort(matched$ain)
-# "5827014035" "5831016035" "5831016036" "5841007024" "5842008018" "5843023069" "5843023070"
+# "5827014035" "5830016058" "5831016035" "5831016036" "5841007024" 
+# "5842008018" "5843023069" "5843023070" "5844011016"
 
-leftover <- mismatch_1 %>% filter(!(ain %in% unique(calfire_matches$ain))) # 5
+leftover <- mismatch_1 %>% filter(!(ain %in% unique(calfire_matches$ain))) # 7
 sort(leftover$ain)
 # "5827014036" "5839016025" "5839016026" "5842008017" "5863003900"
 
@@ -298,7 +314,7 @@ mismatch1_damaged <- calfire_matches %>%
 
 # Review matches with significant damage and check if APN is more accurate (using assessor portal map): 
 # https://portal.assessor.lacounty.gov/mapsearch
-mismatch1_damaged$apn_parcel # 8
+mismatch1_damaged$apn_parcel # 10
 # apn from calfire / assessor shp AIN / use code/ assessor URL
 # "5843023037" / "5843023070" - 0101 - https://portal.assessor.lacounty.gov/parceldetail/5843023037
 # "5843023016" / "5843023069" - 0101 (pending delete) - https://portal.assessor.lacounty.gov/parceldetail/5843023016
@@ -308,13 +324,17 @@ mismatch1_damaged$apn_parcel # 8
 # "5831016032" / "5831016036" - 0100 - https://portal.assessor.lacounty.gov/parceldetail/5831016032
 # "5841007017" / "5841007024" - 0100 - https://portal.assessor.lacounty.gov/parceldetail/5841007017
 # "5842008010" / "5842008018" - 0100 (pending delete) - https://portal.assessor.lacounty.gov/parceldetail/5842008010
+# "5844011003" / "5844011016" - 0100 - https://portal.assessor.lacounty.gov/parceldetail/5844011003
+# "5830016052" / "5830016058" - 0101 - https://portal.assessor.lacounty.gov/parceldetail/5830016052
 
 # above confirms that apn_parcel is likely the better ain, 
-# we should check that these are in the csv - they are
-check <- all_csv_results %>% filter(ain %in% mismatch1_damaged$apn_parcel) #6
-n_distinct(mismatch1_damaged$apn_parcel) #6
+# we should check that these are in the csv - they are not
+all_csv_results <- dbGetQuery(conn = con, sprintf("SELECT * FROM %s.%s;", schema, csv_table_name))
+check <- all_csv_results %>% filter(ain %in% mismatch1_damaged$apn_parcel) # 0
+n_distinct(mismatch1_damaged$apn_parcel) # 8
 
-### 2. Get AINs from CSVs (based on situs city_state filter), and review which ones are not in the SHP ain universes
+### 2. Get AINs from CSVs (based on situs city_state filter), 
+### and review which ones are not in the SHP ain universes
 city_list <- c("Altadena")
 
 csv_1_results <- batch_filter_csv_data(
@@ -342,7 +362,7 @@ csv_3_results <- batch_filter_csv_data(
   exact_match=FALSE)
 
 # combine
-all_csv_results <- bind_rows(csv_1_results, csv_2_results, csv_3_results) #11741
+all_csv_results <- bind_rows(csv_1_results, csv_2_results, csv_3_results) #11739
 
 # clean column names
 colnames(all_csv_results)
@@ -351,7 +371,7 @@ colnames(all_csv_results) <- gsub(" ", "_", tolower(colnames(all_csv_results)))
 colnames(all_csv_results)
 
 # unique AIN for each row
-length(unique(all_csv_results$ain)) #11741
+length(unique(all_csv_results$ain)) #11739
 
 # frequency table of situs 'city_state' field
 table(all_csv_results$city_state, useNA = "ifany")
@@ -368,27 +388,33 @@ all_csv_results <- read.csv(paste0("W:/Project/RDA Team/Altadena Recovery and Re
 
 # compare to shp ain universe
 # in csv, but not in shp - prioritize residential and non-vacant
-mismatch_2 <- data.frame(ain=setdiff(all_csv_results$ain, shp_ain_universe)) %>% # 20
+mismatch_2 <- data.frame(ain=setdiff(all_csv_results$ain, shp_ain_universe)) %>% # 24
   left_join(all_csv_results, by="ain") %>%
   mutate(mismatch1_apn = ifelse(ain %in% mismatch1_damaged$apn_parcel, TRUE, FALSE)) %>%
   select(ain, mismatch1_apn, everything())
 
-table(mismatch_2$mismatch1_apn, mismatch_2$use_code) # mismatch 2 includes all the shp ains that matched to damaged structures in calfire data
-table(mismatch_2$use_code) # Note: There are 10 (in addition to the 6 from mismatch1) that are residential and non-vacant
+# mismatch 2 includes all the shp ains that matched to damaged structures in calfire data
+table(mismatch_2$mismatch1_apn, mismatch_2$use_code) 
+# Note: There are 10 (in addition to the 8 from mismatch1) that are residential and non-vacant
+table(mismatch_2$use_code)
 
+# missing parcel shapes but have CSV data and are considered residential, non-vacant by Assessor
 check <- mismatch_2 %>% 
-  filter(mismatch1_apn==FALSE & grepl("^0", use_code) & !grepl("V$", use_code)) # 10
+  filter(mismatch1_apn==FALSE & grepl("^0", use_code) & !grepl("V$", use_code)) # 12
+
 check2 <- check %>% 
   mutate(ain=as.character(ain)) %>% 
   left_join(calfire, by=c("ain"="apn_parcel")) %>%
-  select(ain, damage, everything()) # 12 - all are NA or No Damage
+  select(ain, damage, everything()) # 14 - all are NA or No Damage
+
 # review the NA damage for possibly missed parcels
 check3 <- check2 %>% filter(is.na(damage)) %>%
   select(ain) %>%
   mutate(ain=as.numeric(ain)) %>%
-  left_join(check) # 8
+  left_join(check) # 10
 
 # going to review addresses manually to see what comes out in assessor
+# 5823022006 - map says AIN is 5823022022, no parcel change history, possibly not impacted by Eaton - https://portal.assessor.lacounty.gov/parceldetail/5823022006
 # 5825003060 - AIN the same, no parcel change history, this looks possibly not impacted by Eaton - https://portal.assessor.lacounty.gov/parceldetail/5825003060
 # 5830004002 - similar to above - https://portal.assessor.lacounty.gov/parceldetail/5830004002
 # 5830004003 - similar to above - https://portal.assessor.lacounty.gov/parceldetail/5830004003
@@ -396,12 +422,13 @@ check3 <- check2 %>% filter(is.na(damage)) %>%
 # 5830004005 - similar to above - https://portal.assessor.lacounty.gov/parceldetail/5830004005
 # 5839016004 - similar to above - https://portal.assessor.lacounty.gov/parceldetail/5839016004
 # 5839016012 - similar to above - https://portal.assessor.lacounty.gov/parceldetail/5839016012
+# 5844011004 - map says AIN is 5844011016, no parcel change history, Misfortune & Calamity Status is in progress, no building: https://portal.assessor.lacounty.gov/parceldetail/5844011004
 # 5863003007 - similar to above - https://portal.assessor.lacounty.gov/parceldetail/5863003007
 
 # Conclusion - mismatch type 1 is more of a data quality concern coming from Assessor's Office. 
 # Mismatch 2 will give us all of Mismatch 1 and more but on closer inspection the "more" covers
 # places where city is "Altadena" but were beyond the fire perimeter or considered out of the
-# way of fire damage. This means the NAs from CalFire don't currently appear to be data quality issues
+# way of fire damage. This means the NAs from CalFire don't currently appear to have data quality issues
 # However we should continue to review in future updates (capacity permitting).
 
 
@@ -418,13 +445,17 @@ shp_intersect_3310_clean <- shp_intersect_3310 %>%
 
 check <- shp_intersect_3310_clean %>% filter(shp_ain %in% mismatch_1_replacements$ain) %>% select(shp_ain, ain, everything())
 
-# re-export results
-date_ran <- as.character(Sys.Date()) 
-source <- "Los Angeles County Assessor; Data Dictionary: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\Assessor Data Extract\\FIELD DEF -- SBF.html"
-indicator <- sprintf("%s/%s Parcels that intersect Altadena 2023 place tiger lines.", data_vintage_month, data_vintage_year)
-shp_table_name <- paste("assessor_parcels_universe", update_year, update_month, sep="_")
+# # rename original table to _old
+# rename_query <- sprintf("ALTER TABLE %s.%s RENAME TO %s_old;", schema, shp_table_name, shp_table_name)
+# dbSendQuery(con, rename_query)
 
-
+# # re-export results
+# date_ran <- as.character(Sys.Date()) 
+# source <- "Los Angeles County Assessor; Data Dictionary: W:\\Project\\RDA Team\\Altadena Recovery and Rebuild\\Data\\Assessor Data Extract\\FIELD DEF -- SBF.html"
+# indicator <- sprintf("%s/%s Parcels that intersect Altadena 2023 place tiger lines.", data_vintage_month, data_vintage_year)
+# shp_table_name <- paste("assessor_parcels_universe", update_year, update_month, sep="_")
+# 
+# 
 # export_shpfile(con=con,
 #                df=shp_intersect_3310_clean,
 #                schema=schema,
@@ -446,7 +477,7 @@ shp_ain_universe <- shp_intersect %>% st_drop_geometry() %>% select(ain) %>% pul
 csv_1_ain_filter <- batch_filter_csv_data(
   csv_file=csv_1,
   target_list = shp_ain_universe,
-  filter_column="ï»¿AIN", # AIN, sometimes has encoding typos
+  filter_column="AIN", # AIN or ï»¿AIN sometimes has encoding typos
   chunk_size = 10000,
   debug_filter=TRUE,
   exact_match=TRUE)
@@ -454,7 +485,7 @@ csv_1_ain_filter <- batch_filter_csv_data(
 csv_2_ain_filter <- batch_filter_csv_data(
   csv_file=csv_2,
   target_list = shp_ain_universe,
-  filter_column="ï»¿AIN",
+  filter_column="AIN",
   chunk_size = 10000,
   debug_filter=TRUE,
   exact_match=TRUE)
@@ -462,7 +493,7 @@ csv_2_ain_filter <- batch_filter_csv_data(
 csv_3_ain_filter <- batch_filter_csv_data(
   csv_file=csv_3,
   target_list = shp_ain_universe,
-  filter_column="ï»¿AIN",
+  filter_column="AIN",
   chunk_size = 10000,
   debug_filter=TRUE,
   exact_match=TRUE)
@@ -470,12 +501,12 @@ csv_3_ain_filter <- batch_filter_csv_data(
 # Combine results
 csv_ains_combined <- rbind(csv_1_ain_filter,
                            csv_2_ain_filter,
-                           csv_3_ain_filter) # 14522
+                           csv_3_ain_filter) # 14519
 
 colnames(csv_ains_combined) <- tolower(gsub(" ", "_", colnames(csv_ains_combined)))
 
 csv_ains_combined <- csv_ains_combined %>%
-  rename(ain = `ï»¿ain`) %>%
+  # rename(ain = `ï»¿ain`) %>%
   mutate(ain=as.character(ain))
 
 length(unique(csv_ains_combined$ain)) # 14522
@@ -483,18 +514,22 @@ length(unique(csv_ains_combined$ain)) # 14522
 # check last sale date
 max(as.Date(as.character(csv_ains_combined$last_sale_date),
             format = "%Y%m%d"),
-    na.rm = TRUE) # "2025-12-31" 
+    na.rm = TRUE) # "2026-05-22" 
+
+# # rename to _old
+# rename_query <- sprintf("ALTER TABLE %s.%s RENAME TO %s_old;", schema, csv_table_name, csv_table_name)
+# dbSendQuery(con, rename_query)
 
 ### Export to postgres
-csv_table_name <- paste("assessor_data_universe", update_year, update_month, sep="_")
-indicator <- sprintf("Assessor data from %s/%s that matches Altadena and Pasadena parcels in %s table.", data_vintage_month, data_vintage_year, shp_table_name)
-dbWriteTable(con, Id(schema, csv_table_name), csv_ains_combined,
-             overwrite = FALSE, row.names = FALSE)
-dbSendQuery(con, paste0("COMMENT ON TABLE ", schema, ".", csv_table_name, " IS '", indicator, "
-            Data imported on ", date_ran, ".",
-                        "QA DOC: ", qa_filepath,
-                        " Source: ", source, "'"))
-
+# csv_table_name <- paste("assessor_data_universe", update_year, update_month, sep="_")
+# indicator <- sprintf("Assessor data from %s/%s that matches Altadena and Pasadena parcels in %s table.", data_vintage_month, data_vintage_year, shp_table_name)
+# dbWriteTable(con, Id(schema, csv_table_name), csv_ains_combined,
+#              overwrite = FALSE, row.names = FALSE)
+# dbSendQuery(con, paste0("COMMENT ON TABLE ", schema, ".", csv_table_name, " IS '", indicator, "
+#             Data imported on ", date_ran, ".",
+#                         "QA DOC: ", qa_filepath,
+#                         " Source: ", source, "'"))
+# 
 
 
 ##### QA Clean-up #####
